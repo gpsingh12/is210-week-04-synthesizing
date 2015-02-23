ef290a9b0e7bae7df916a15820e11524836da9a8
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-23 02:02:30 +0000

    edit2

--- task_03.py
+++ task_03.py
@@ -2,65 +2,62 @@
 # -*- coding: utf-8 -*-
 """ Docstring. """
 
-import decimal
-import fractions
-import math
 NAME = raw_input('What is your name? ')
 
 PRINCIPAL = raw_input('What is amount of your Principal? $ ')
 PRINCIPAL = int(PRINCIPAL)
-PRN = PRINCIPAL <= 199,999
-PRP = (2000,000 <= PRINCIPAL <= 999,999)
+PRN = PRINCIPAL <= 199999
+PRP = (2000000 <= PRINCIPAL <= 999999)
 PRL = (PRINCIPAL >= 1000000)
 DURATION = raw_input('for how many years is this loan being borrowed? ')
 DURATION = int(DURATION)
-DUR =  (DURATION <= 15)
-DRN = (16 <= DURATION <=20)
+DUR = (DURATION <= 15)
+DRN = (16 <= DURATION <= 20)
 DRT = (21 <= DURATION <= 30)
-Prequalified = raw_input('Are you pre-qualified for this loan? ')
-PRE_Q = Prequalified == 'Yes' or Prequalified == 'y'
-PRE_QL = Prequalified == 'No' or 'n'
+Pre-qualified = raw_input('Are you pre-qualified for this loan? ')
+PRE_Q = Pre-qualified == 'Yes' or Prequalified == 'y'
+PRE_QL = Pre-qualified == 'No' or 'n'
 
 if PRN and DUR and PRE_Q:
-    InterestRate = 3.63
+    Interest Rate = 3.63
 else:
-    InterestRate = 4.65
+    Interest Rate = 4.65
 
 if PRN and DRN and PRE_Q :
-    InterestRate = 4.04
+    Interest Rate = 4.04
 else:
-    InterestRate = 4.98
+    Interest Rate = 4.98
 
 if PRN and PRE_Q and DRT:
-    InterestRate = 5.77
+    Interest Rate = 5.77
 else:
-    Interestrate = 6.39
+    Interest Rate = 6.39
 
 if PRP and PRE_Q and DUR:
-    InterestRate = 3.02 
+    Interest Rate = 3.02 
 else:
-    InterestRate = 3.98
+    Interest Rate = 3.98
 
 if PRP and DRN and PRE_Q:
-    InterestRate = 3.27
+    Interest Rate = 3.27
 else:
-    InterestRate = 4.08
+    Interest Rate = 4.08
 
 if PRP and DRT and PRE_Q:
-    InterestRate = 4.66
+    Interest Rate = 4.66
 
 
 if PRL and DUR and PRE_Q:
-    InterestRate = 2.05
+    Interest Rate = 2.05
 
 if PRL and DRN and PRE_Q:
-    InterestRate = 2.62
+    Interest Rate = 2.62
 
     
-AMOUNT = PRINCIPAL *( 1 + (InterestRate/12))**float(12*DURATION)
+AMOUNT = PRINCIPAL *(1 + (InterestRate/12))**float(12*DURATION)
 AM = format(AMOUNT, '.5f')
-print InterestRate   
-print '                                    '               
+print Interest Rate   
+#print '                                    '               
 print 'Loan Report for: ' '{}'.format(NAME)
 print '--------------------------------------'
 print 'Principal : ''{}'.format(PRINCIPAL)
@@ -68,6 +65,3 @@ print 'Duration: ' '{}'.format(DURATION)
 print 'Prequalified: ' '{}'.format(Prequalified)
 print '                                          '
 print 'Total: ''{}'.format(AM)
-
-
-
